8921014438388eda5107ce83ff667461abeddc25
Jeff Mentch <jsmentch@gmail.com>
2020-10-10 16:30:44 -0400

    add librosa resampling and pad script

---
+++ scripts/resample_pad_librosa.py
@@ -0,0 +1,18 @@
+import sys
+import numpy
+import soundfile
+import librosa
+
+def main():
+    fs_new = 22050
+    length_new = 7
+    for f in sys.argv[1:]:
+        y, fs = soundfile.read(f)
+        y_22k = librosa.resample(y, fs, fs_new) #resample to 22.05k
+        y_22k_pad = librosa.util.fix_length(y_22k, fs_new*length_new) #pad to length_new seconds
+        soundfile.write(f, y_22k_pad, fs_new, subtype='PCM_16')
+        print("Processed and wrote %s." % f)
+
+
+if __name__ == "__main__":
+    main()
