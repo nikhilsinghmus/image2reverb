108adb793da1aff383b9d58c01d388984f4fc8d4
Jeff Mentch <jsmentch@gmail.com>
2020-10-15 21:07:17 -0400

    change length of files so that resulting spectrograms will be 512x512

--- scripts/resample_pad_librosa.py
+++ scripts/resample_pad_librosa.py
@@ -5,11 +5,11 @@ import librosa
 
 def main():
     fs_new = 22050
-    length_new = 7
+    length_new = 5.94 # to make it resulting spectrograms 512x512
     for f in sys.argv[1:]:
         y, fs = soundfile.read(f)
         y_22k = librosa.resample(y, fs, fs_new) #resample to 22.05k
-        y_22k_pad = librosa.util.fix_length(y_22k, fs_new*length_new) #pad to length_new seconds
+        y_22k_pad = librosa.util.fix_length(y_22k, round(fs_new*length_new)) #pad to length_new seconds
         soundfile.write(f, y_22k_pad, fs_new, subtype='PCM_16')
         print("Processed and wrote %s." % f)
 
