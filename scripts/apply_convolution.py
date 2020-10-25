4226bcd0ebc1fd8833120875f5d2a33599190de9
Nikhil Singh <nikhilsinghmus@gmail.com>
2020-10-25 18:58:07 -0400

    Add script to apply convolution to a file.

--- scripts/apply_convolution.py
+++ scripts/apply_convolution.py
@@ -1,4 +1,5 @@
 import sys
+import subprocess
 import numpy
 import scipy.signal
 import soundfile
@@ -6,14 +7,22 @@ import librosa
 
 
 def main():
-    audio, reverb, output = sys.argv[1:]
+    audio = sys.argv[1]
+    example = sys.argv[2]
+    reverb = "../dataset/test_latest/%s_label_synthesized_image.wav" % example
+    s = audio.split("/")
+    output = "output/test_%s_%s" % (example, s[s.index("Anechoic") + 1])
     y, sr = librosa.load(audio)
-    r = apply_reverb(y, reverb)
-    soundfile.write(output, r, sr)
+    r = apply_reverb(y, reverb, r = len(sys.argv) > 3)
+    soundfile.write(output + ".wav", r, sr)
+
+    subprocess.run(["ffmpeg", "-loop", "1", "-i", "../dataset/test_latest/inputs/%s_label_input_label.jpg" % example, "-i", output + ".wav", "-c:v", "libx264", "-tune", "stillimage", "-c:a", "aac", "-b:a", "192k", "-pix_fmt", "yuv420p", "-shortest", output + ".mp4"])
 
 
-def apply_reverb(signal: numpy.ndarray, path: str, mix: float = 0.02) -> numpy.ndarray:
+def apply_reverb(signal: numpy.ndarray, path: str, mix: float = 0.02, r = False) -> numpy.ndarray:
     reverb, _ = soundfile.read(path)
+    if r:
+        reverb = reverb[::-1]
     signal = numpy.concatenate((signal, numpy.zeros(reverb.shape)))
     reverb /= numpy.max(numpy.abs(reverb))
     signal += scipy.signal.oaconvolve(signal, reverb, "full")[:len(signal)] * mix
