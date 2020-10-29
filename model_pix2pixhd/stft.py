93d4cdfe418d03bec280f17a745f300680f85758
Nikhil Singh <nikhilsinghmus@gmail.com>
2020-10-28 21:04:40 -0400

    Add windowing and comments.

--- model_pix2pixhd/stft.py
+++ model_pix2pixhd/stft.py
@@ -8,25 +8,24 @@ M_PI = 3.14159265358979323846264338
 class STFT(torch.nn.Module):
     def __init__(self, window_size=1024, hop_length=256, window="hann"):
         super().__init__()
-
         self._w_size = window_size
         self._h_length = hop_length
-        self._w = {"hann": torch.hann_window}[window](self._w_size).cuda()
+        self._w = {"hann": torch.hann_window}[window](self._w_size) # Window table
 
     def transform(self, audio):
-        s = torch.stft(audio, self._w_size, self._h_length, return_complex=True).squeeze()[:-1,:]
-        m = torch.abs(s)
-        phase_angle = torch.angle(s)
-        i_f = phase_angle[:,1:] - phase_angle[:,:-1]
+        s = torch.stft(audio, self._w_size, self._h_length, window=self._w, return_complex=True).squeeze()[:-1,:] # Get STFT and trim Nyquist bin
+        m = torch.abs(s) # Magnitude
+        phase_angle = torch.angle(s) # Phase angle
+        i_f = phase_angle[:,1:] - phase_angle[:,:-1] # Finite difference
         i_f = torch.where(i_f > M_PI, i_f - 2 * M_PI, i_f)
         i_f = torch.where(i_f < M_PI, i_f + 2 * M_PI, i_f)
         i_f = torch.cat((phase_angle[:,:1], i_f), axis=1)
-        return torch.stack((torch.log(m), i_f)).cuda()
+        return torch.stack((torch.log(m), i_f)) # (2, 512, 512) output (log magnitude)
 
     def inverse(self, spec):
-        m, i_f = spec
-        m = torch.exp(m)
+        m, i_f = spec # (2, 512, 512) input
+        m = torch.exp(m) # Linear magnitude
         phase_angle = torch.cumsum(i_f, 1)
-        phase = torch.cos(phase_angle) + (torch.sin(phase_angle) * 1j)
-        s = torch.cat((m * phase, torch.zeros((1, m.shape[1])).cuda()), axis=0)
-        return torch.istft(s, self._w_size, self._h_length)
+        phase = torch.cos(phase_angle) + (torch.sin(phase_angle) * 1j) # Cartopol, basically
+        s = torch.cat((m * phase, torch.zeros((1, m.shape[1]))), axis=0) # Zero-pad for Nyquist bin
+        return torch.istft(s, self._w_size, self._h_length, window=self._w) # Audio output
