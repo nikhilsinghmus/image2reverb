590325e6da7b392ee1367545657aabd1ec196602
Nikhil Singh <nikhilsinghmus@gmail.com>
2020-10-29 20:06:30 -0400

    Important corrections to STFT in both.

--- model_pix2pixhd/stft.py
+++ model_pix2pixhd/stft.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision.transforms
 import torchaudio
 
 
@@ -11,21 +12,24 @@ class STFT(torch.nn.Module):
         self._w_size = window_size
         self._h_length = hop_length
         self._w = {"hann": torch.hann_window}[window](self._w_size) # Window table
+        self._n = torchvision.transforms.Normalize((0.5, 0.5), (0.5, 0.5))
 
     def transform(self, audio):
         s = torch.stft(audio, self._w_size, self._h_length, window=self._w, return_complex=True).squeeze()[:-1,:] # Get STFT and trim Nyquist bin
         m = torch.abs(s) # Magnitude
         phase_angle = torch.angle(s) # Phase angle
         i_f = phase_angle[:,1:] - phase_angle[:,:-1] # Finite difference
-        i_f = torch.where(i_f > M_PI, i_f - 2 * M_PI, i_f)
-        i_f = torch.where(i_f < M_PI, i_f + 2 * M_PI, i_f)
         i_f = torch.cat((phase_angle[:,:1], i_f), axis=1)
-        return torch.stack((torch.log(m + 1.0e-6), i_f)) # (2, 512, 512) output (log magnitude)
+        i_f = torch.where(i_f > M_PI, i_f - 2 * M_PI, i_f)
+        i_f = torch.where(i_f < -M_PI, i_f + 2 * M_PI, i_f)
+        m = torch.log(m + 1.0e-6)
+        return self._n(torch.stack((m, i_f))) # (2, 512, 512) output (log magnitude)
 
     def inverse(self, spec):
         m, i_f = spec # (2, 512, 512) input
         m = torch.exp(m) # Linear magnitude
-        phase_angle = torch.cumsum(i_f, 1)
+        phase_angle = torch.cumsum(i_f, 1) * M_PI
         phase = torch.cos(phase_angle) + (torch.sin(phase_angle) * 1j) # Cartopol, basically
         s = torch.cat((m * phase, torch.zeros((1, m.shape[1]))), axis=0) # Zero-pad for Nyquist bin
-        return torch.istft(s, self._w_size, self._h_length, window=self._w) # Audio output
+        audio = torch.istft(s, self._w_size, self._h_length, window=self._w) # Audio output
+        return audio/torch.abs(audio).max()
