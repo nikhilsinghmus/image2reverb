c82df16df885383abe3713d87bf4d60b123faa36
Nikhil Singh <nikhilsinghmus@gmail.com>
2020-10-30 20:00:57 -0400

    Modify convolution script to accept data directory arg.

--- scripts/apply_convolution.py
+++ scripts/apply_convolution.py
@@ -9,14 +9,15 @@ import librosa
 def main():
     audio = sys.argv[1]
     example = sys.argv[2]
-    reverb = "../dataset/test_latest/%s_label_synthesized_image.wav" % example
+    data_dir = sys.argv[3]
+    reverb = os.path.join(data_dir, "%s_label_synthesized_image.wav" % example)
     s = audio.split("/")
     output = "output/test_%s_%s" % (example, s[s.index("Anechoic") + 1])
     y, sr = librosa.load(audio)
     r = apply_reverb(y, reverb, r = len(sys.argv) > 3)
     soundfile.write(output + ".wav", r, sr)
 
-    subprocess.run(["ffmpeg", "-loop", "1", "-i", "../dataset/test_latest/inputs/%s_label_input_label.jpg" % example, "-i", output + ".wav", "-c:v", "libx264", "-tune", "stillimage", "-c:a", "aac", "-b:a", "192k", "-pix_fmt", "yuv420p", "-shortest", output + ".mp4"])
+    subprocess.run(["ffmpeg", "-loop", "1", "-i", os.path.join(data_dir, "inputs/%s_label_input_label.jpg" % example), "-i", output + ".wav", "-c:v", "libx264", "-tune", "stillimage", "-c:a", "aac", "-b:a", "192k", "-pix_fmt", "yuv420p", "-shortest", output + ".mp4"])
 
 
 def apply_reverb(signal: numpy.ndarray, path: str, mix: float = 0.02, r = False) -> numpy.ndarray:
