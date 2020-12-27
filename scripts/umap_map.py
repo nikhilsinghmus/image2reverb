d6622ebf0c155c3a72ef818db508b0afc5df6cdf
Nikhil Singh <nikhilsinghmus@gmail.com>
2020-12-27 17:28:30 -0500

    Switch to UMAP for dataset map from T-SNE.

--- scripts/umap_map.py
+++ scripts/umap_map.py
@@ -4,7 +4,7 @@ import numpy
 import scipy.spatial
 import soundfile
 import sklearn.preprocessing
-import sklearn.manifold
+import umap
 import acoustics
 from matplotlib import pyplot
 from lapjv import lapjv
@@ -14,42 +14,49 @@ from PIL import Image
 def main():
     audio_dir = sys.argv[1]
     image_dir = sys.argv[2]
-    files = (os.path.join(audio_dir, f) for f in sorted(os.listdir(audio_dir)) if os.path.splitext(f)[1] == ".wav")
+    files = [os.path.join(audio_dir, f) for f in sorted(os.listdir(audio_dir)) if os.path.splitext(f)[1] == ".wav"]
     images = (os.path.join(image_dir, f) for f in sorted(os.listdir(image_dir)) if os.path.splitext(f)[1]  in [".png", ".jpg"])
 
+    load_audio = True
     audio = []
+    if os.path.isfile("audio.npy"):
+        audio = numpy.load("audio.npy")
+        load_audio = False
     imgs = []
     bands = acoustics.standards.iec_61672_1_2013.NOMINAL_OCTAVE_CENTER_FREQUENCIES[2:-4]
     print("Data loading.")
-    for audio_f, image_f in zip(files, images):
+    m = len(files)
+    for i, (audio_f, image_f) in enumerate(zip(files, images)):
+        print("Processing %d of %d." % (i + 1, m))
         assert os.path.basename(audio_f[:audio_f.rfind("_")]) == os.path.basename(image_f[:image_f.rfind("_")])
-        audio.append(acoustics.room.t60_impulse(audio_f, bands, rt="edt"))
         imgs.append(Image.open(image_f).convert("RGB"))
+        if not load_audio:
+            continue
+        audio.append(acoustics.room.t60_impulse(audio_f, bands))
 
-    # print("Scaling and PCA to 10 dimensions.")
-    audio = numpy.array(audio)
-    # audio_input = sklearn.preprocessing.scale(audio)
-    # pca_f = sklearn.decomposition.PCA(n_components=10).fit_transform(audio_input)
+    if load_audio:
+        audio = numpy.array(audio)
+        numpy.save("audio", audio)
 
     w = 26
     h = 42
 
-    print("T-SNE to 2 dimensions.")
-    tsne_f = generate_tsne(audio)
+    print("UMAP to 2 dimensions.")
+    f = generate_umap(audio)
 
     print("Make grid and store.")
-    save_tsne_grid(imgs, tsne_f, w, h)
+    save_grid(imgs, f, w, h)
 
 
-def generate_tsne(X):
-    tsne = sklearn.manifold.TSNE(n_components=2)
-    X_2d = tsne.fit_transform(X)
+def generate_umap(X):
+    u = umap.UMAP(n_components=2)
+    X_2d = u.fit_transform(X)
     X_2d -= X_2d.min(axis=0)
     X_2d /= X_2d.max(axis=0)
     return X_2d
 
 
-def save_tsne_grid(img_collection, X_2d, w, h, out_res=224):
+def save_grid(img_collection, X_2d, w, h, out_res=224):
     grid = numpy.dstack(numpy.meshgrid(numpy.linspace(0, 1, w), numpy.linspace(0, 1, h))).reshape(-1, 2)
     cost_matrix = scipy.spatial.distance.cdist(grid, X_2d, "sqeuclidean").astype(numpy.float32)
     cost_matrix = cost_matrix * (100000 / cost_matrix.max())
