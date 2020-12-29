07bd2f8330e2cadd854196c7b649955cad00398b
Nikhil Singh <nikhilsinghmus@gmail.com>
2020-12-29 00:11:57 -0500

    Add initial monodepth2 functionality to Encoder.

--- model/networks.py
+++ model/networks.py
@@ -1,33 +1,49 @@
+import os
 import torch
 import torch.nn as nn
 import torchvision.models as models
 from torch.autograd import Variable
 from torchvision import transforms
 from .layers import PixelWiseNormLayer, MiniBatchAverageLayer, EqualizedLearningRateLayer
+from .resnet_encoder import ResnetEncoder
+from .depth_decoder import DepthDecoder
 
 
 class Encoder(nn.Module):
     """Load encoder from pre-trained ResNet50 (places365 CNNs) model. Link: http://places2.csail.mit.edu/models_places365/resnet50_places365.pth.tar"""
-    def __init__(self, model_weights, device="cuda"):
+    def __init__(self, model_weights, depth_model, device="cuda"):
         super().__init__()
         self.model_weights = model_weights
         self.model = models.resnet50(num_classes=365)
         c = torch.load(model_weights, map_location=device)
         state_dict = {k.replace("module.", ""): v for k, v in c["state_dict"].items()}
         self.model.load_state_dict(state_dict)
+        f = self.model.conv1.weight
+        self.model.conv1.weight = torch.nn.Parameter(torch.cat((f, torch.randn(64, 1, 7, 7)), 1))
         self.model.to(torch.device(device))
         self.model.train()
 
+        encoder_path = os.path.join(depth_model, "encoder.pth")
+        depth_decoder_path = os.path.join(depth_model, "depth.pth")
+        self.depth_encoder = ResnetEncoder(18, False)
+        loaded_dict_enc = torch.load(encoder_path, map_location=device)
+
+        self.feed_height = loaded_dict_enc["height"]
+        self.feed_width = loaded_dict_enc["width"]
+        filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if k in self.depth_encoder.state_dict()}
+        self.depth_encoder.load_state_dict(filtered_dict_enc)
+        self.depth_encoder.to(device)
+        self.depth_encoder.eval()
+
+        self.depth_decoder = DepthDecoder(num_ch_enc=self.depth_encoder.num_ch_enc, scales=range(4))
+        loaded_dict = torch.load(depth_decoder_path, map_location=device)
+        self.depth_decoder.load_state_dict(loaded_dict)
+        self.depth_decoder.to(device)
+        self.depth_decoder.eval()
+
     def forward(self, x):
+        x = torch.cat((x, list(self.depth_decoder(self.depth_encoder(x)).values())[-1]), 1)
         return self.model.forward(x).unsqueeze(-1).unsqueeze(-1)
-    
-    def recursion_change_bn(self, module):
-        if isinstance(module, torch.nn.BatchNorm2d):
-            module.track_running_stats = 1
-        else:
-            for i, (name, module1) in enumerate(module._modules.items()):
-                module1 = self.recursion_change_bn(module1)
-        return module
 
 
 class Generator(nn.Module):
